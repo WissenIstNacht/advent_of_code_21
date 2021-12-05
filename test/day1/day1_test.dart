3ec61c2f4fd26a919ebe4a70426e5d78df39db91
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-05 19:44:08 +0100

    d1p1 solve test

--- test/day1/day1_test.dart
+++ test/day1/day1_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:test/test.dart';
 import 'package:advent_of_code_21/solver.dart';
 
@@ -5,13 +7,13 @@ import '../../bin/day1/d1p1.dart';
 
 void main() {
   late Solver solver;
+  late List<String> sampleInput;
   setUp(() {
     solver = Day1Part1();
+    sampleInput = File("test/day1/inputs/sample.txt").readAsLinesSync();
   });
-
-  test('test description', () {
-    // test
-    expect(2 * 2 * 2, equals(8));
+  test("test d1p1's solve method", () {
+    final res = solver.solve(sampleInput);
+    expect(res, equals("7"));
   });
-  //further tests
 }
