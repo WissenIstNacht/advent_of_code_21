8d66a09143055886acb6252404b09334f9a31811
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-05 21:00:17 +0100

    scaffold day 2 and add input

---
+++ bin/day2/p1.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day2Part1 extends Solver {
+  Day2Part1() : super(1);
+
+  @override
+  String solve(List<String> input) {
+    return "hi 1";
+  }
+}
