3fd8916db7456ae71d9bf3858baf3a7c41314ba9
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-05 20:02:04 +0100

    d1p2 scaffold

---
+++ bin/day1/d1p2.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day1Part2 extends Solver {
+  Day1Part2() : super(1);
+
+  @override
+  String solve(List<String> input) {
+    return "hi";
+  }
+}
--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,8 @@
 import 'package:advent_of_code_21/executor.dart';
 
-import 'day1/d1p1.dart';
+import 'day1/d1p2.dart';
 
 void main(List<String> arguments) {
-  var solver = Day1Part1();
+  var solver = Day1Part2();
   Executor.execute(solver);
 }
