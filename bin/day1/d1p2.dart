f0aa2c6d77b6e1d7cd939c8ac44e91a6d97da698
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-05 20:27:50 +0100

    solution for d1p2

--- bin/day1/d1p2.dart
+++ bin/day1/d1p2.dart
@@ -1,10 +1,22 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'd1p1.dart';
+
 class Day1Part2 extends Solver {
   Day1Part2() : super(1);
 
   @override
   String solve(List<String> input) {
-    return "hi";
+    final numberInput = input.map(int.parse).toList();
+    final windowSums = <int>[];
+
+    for (var i = 0; i < numberInput.length - 2; i++) {
+      final window = numberInput.sublist(i, i + 3);
+      final sum = window.reduce((a, b) => a + b);
+      windowSums.add(sum);
+    }
+
+    final res = Day1Part1.countIncreases(windowSums);
+    return res.toString();
   }
 }
