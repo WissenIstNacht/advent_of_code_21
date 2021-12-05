89a035fcb55bcc18d5e0984de07e8530025d6ec5
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-05 20:36:47 +0100

    minor refactor of util library

--- lib/executor.dart
+++ lib/executor.dart
@@ -4,9 +4,9 @@ import 'utils.dart';
 class Executor {
   static void execute(Solver solver) {
     print("Starting Program ---");
-    var input = Utils.readInput(solver.day);
+    var input = readInput(solver.day);
     var output = solver.solve(input);
-    Utils.prettyPrint(output);
+    prettyPrint(output);
     print("Ending Program ---");
   }
 }
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,11 +1,9 @@
 import 'dart:io';
 
-class Utils {
-  static List<String> readInput(int problem) {
-    return File('bin/day$problem/input.txt').readAsLinesSync();
-  }
+List<String> readInput(int problem) {
+  return File('bin/day$problem/input.txt').readAsLinesSync();
+}
 
-  static void prettyPrint(String result) {
-    print("The result is: $result");
-  }
+void prettyPrint(String result) {
+  print("The result is: $result");
 }
