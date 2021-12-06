48433e9539efab0a5ce3266d700174df86fcc7d6
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-06 21:59:43 +0100

    prep d3

---
+++ bin/day3/p2.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day3Part2 extends Solver {
+  Day3Part2() : super(3);
+
+  @override
+  String solve(List<String> input) {
+    return "res 2";
+  }
+}
