fcf9db3b35f6e91ee2c29d8cc8155dfb156d7ea3
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-06 21:49:06 +0100

    test d2p2

--- test/day2/day2_test.dart
+++ test/day2/day2_test.dart
@@ -3,7 +3,7 @@ import 'package:advent_of_code_21/solver.dart';
 import 'package:test/test.dart';
 
 import '../../bin/day2/p1.dart';
-// import '../../bin/day2/p2.dart';
+import '../../bin/day2/p2.dart';
 
 void main() {
   late Solver solver;
@@ -11,14 +11,14 @@ void main() {
   setUp(() {
     sampleInput = readSampleInput(2);
   });
-  test("test d1p1's solve method", () {
+  test("test d2p1's solve method", () {
     solver = Day2Part1();
     final res = solver.solve(sampleInput);
     expect(res, equals("150"));
   });
-  // test("test d1p2's solve method", () {
-  //   solver = Day1Part2();
-  //   final res = solver.solve(sampleInput);
-  //   expect(res, equals("5"));
-  // });
+  test("test d2p2's solve method", () {
+    solver = Day2Part2();
+    final res = solver.solve(sampleInput);
+    expect(res, equals("900"));
+  });
 }
