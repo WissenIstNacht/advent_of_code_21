7f76470bc3bf564c4f9cd6f95aa0d9bbfeb557e7
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-06 21:46:12 +0100

    implement solution for d2p2

--- bin/day2/p2.dart
+++ bin/day2/p2.dart
@@ -1,10 +1,17 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'instruction.dart';
+import 'position.dart';
+
 class Day2Part2 extends Solver {
-  Day2Part2() : super(1);
+  Day2Part2() : super(2);
 
   @override
   String solve(List<String> input) {
-    return "hi2";
+    final instrs = input.map((s) => Instruction.parse(s));
+    final pos = Position2();
+    instrs.forEach(pos.move);
+    final res = pos.depth * pos.horizontal;
+    return res.toString();
   }
 }
