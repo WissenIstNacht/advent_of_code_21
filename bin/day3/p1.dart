1b0b0756a906c42d1124c45ec4ce9e7806ffa3cb
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-08 17:57:44 +0100

    implement solution for d3p1

--- bin/day3/p1.dart
+++ bin/day3/p1.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'package:collection/collection.dart';
+
 import 'package:advent_of_code_21/solver.dart';
 
 class Day3Part1 extends Solver {
@@ -5,6 +8,27 @@ class Day3Part1 extends Solver {
 
   @override
   String solve(List<String> input) {
-    return "res 1";
+    final s = input.first;
+    final rows = input.length;
+    final cols = s.length;
+    final acc = List<int>.filled(cols, 0);
+
+    List<int> folder(List<int> acc, String val) {
+      val.runes.forEachIndexed((index, elem) {
+        if (elem.toString() == "1") {
+          acc[index] += 1;
+        }
+      });
+      return acc;
+    }
+
+    input.fold(acc, folder);
+
+    final gammaArray = acc.map((n) => (n / rows).round());
+    final gamma = gammaArray
+        .mapIndexed((index, elem) => elem == 1 ? pow(2, cols - index) : 0)
+        .reduce((a, b) => a + b);
+    final epsilon = pow(2, cols) - (1 + gamma);
+    return (gamma * epsilon).toString();
   }
 }
