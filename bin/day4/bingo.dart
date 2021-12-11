80755d3dedc5fc7d8ceb63d680333aa464ca6214
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-11 16:13:55 +0100

    fix auxiliary classes

--- bin/day4/bingo.dart
+++ bin/day4/bingo.dart
@@ -6,16 +6,23 @@ class Bingo {
 
   Bingo.fromInput(List<String> input) {
     final picks = input.first;
-    numbers = picks.split(" ").map(int.parse);
+    numbers = picks.split(",").map(int.parse);
+    if (numbers.isEmpty) {
+      throw ArgumentError("The game has no numbers picked");
+    }
 
     var i = 2;
+    var newBoards = <Board>[];
     while (i < input.length) {
       final boardLines = input.getRange(i, i + 5);
-      boards.add(Board.fromLines(boardLines));
+      newBoards.add(Board.fromLines(boardLines));
       i += 6;
     }
+    if (newBoards.isEmpty) {
+      throw ArgumentError("The game has no boards");
+    }
+    boards = newBoards;
   }
-
   void play() {
     return;
   }
--- bin/day4/board.dart
+++ bin/day4/board.dart
@@ -4,9 +4,18 @@ class Board {
   late Iterable<Iterable<Cell>> cells;
 
   Board.fromLines(Iterable<String> lines) {
+    if (lines.length != 5) {
+      throw ArgumentError("Board input does not have 5 rows");
+    }
     final cols = <Iterable<Cell>>[];
     for (var s in lines) {
-      final numbers = s.split(" ");
+      final exp = RegExp("\\s+");
+      final numbers = s.trim().split(exp);
+      // final numbers = s.split(" ");
+      if (numbers.length != 5) {
+        throw ArgumentError(
+            "Board input has a row that does not have 5 columns");
+      }
       final row = numbers.map((s) => Cell.fromString(s));
       cols.add(row);
     }
