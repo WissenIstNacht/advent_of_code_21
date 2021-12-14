e89eb6090e6cfa56c8b2d19b9cea525d55a4cbf8
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-14 21:01:58 +0100

    implement solution d4p2

--- bin/day4/p2.dart
+++ bin/day4/p2.dart
@@ -1,10 +1,26 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'bingo.dart';
+
 class Day4Part2 extends Solver {
   Day4Part2() : super(4);
 
   @override
   String solve(List<String> input) {
-    return "2";
+    final game = Bingo.fromInput(input);
+    var gamesWon = 0;
+    var lastBoard;
+    var currPick;
+
+    while (gamesWon < game.boards.length) {
+      final currPick = game.pickNumber();
+      game.updateBoards(currPick);
+      final winner = game.hasWinningBoard();
+      if (winner != null) {
+        gamesWon++;
+        lastBoard = winner;
+      }
+    }
+    return (lastBoard.score() * currPick).toString();
   }
 }
