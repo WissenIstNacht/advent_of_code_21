8391059b8a0bd05f73e30aafbe67c93801985043
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-15 20:07:00 +0100

    setup d5

---
+++ bin/day5/p1.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day4Part1 extends Solver {
+  Day4Part1() : super(5);
+
+  @override
+  String solve(List<String> input) {
+    return "1";
+  }
+}
