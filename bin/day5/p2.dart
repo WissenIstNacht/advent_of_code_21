b560180c7b6e7d5dfb88f43fa8279b5f8904aa00
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-17 20:19:44 +0100

    implement solution for d5p2

--- bin/day5/p2.dart
+++ bin/day5/p2.dart
@@ -1,10 +1,20 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'grid.dart';
+import 'line.dart';
+import 'utils.dart';
+
 class Day5Part2 extends Solver {
   Day5Part2() : super(5);
 
   @override
   String solve(List<String> input) {
-    return "2";
+    final lines = input.map((s) => Line.fromString(s));
+
+    final dimensions = findDimensions(lines);
+    final grid = Grid.fromDimension(dimensions.x, dimensions.y);
+
+    lines.forEach((line) => grid.markLine(line));
+    return grid.countOverlaps().toString();
   }
 }
