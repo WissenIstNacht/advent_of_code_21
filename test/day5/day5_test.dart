fdfc0848da2171761c4279c8119fc3458eecc241
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-17 20:21:21 +0100

    test d5p2

--- test/day5/day5_test.dart
+++ test/day5/day5_test.dart
@@ -17,9 +17,9 @@ void main() {
     print(res);
     expect(res, equals("5"));
   });
-  // test("test d4p2's solve method", () {
-  //   solver = Day5Part2();
-  //   final res = solver.solve(sampleInput);
-  //   expect(res, equals("1"));
-  // });
+  test("test d5p2's solve method", () {
+    solver = Day5Part2();
+    final res = solver.solve(sampleInput);
+    expect(res, equals("12"));
+  });
 }
