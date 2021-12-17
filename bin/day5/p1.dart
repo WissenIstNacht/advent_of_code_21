d4eb147c32c0ccf685460649485516ebcd789d73
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-17 10:03:36 +0100

    implement solution for d5p1

--- bin/day5/p1.dart
+++ bin/day5/p1.dart
@@ -1,10 +1,26 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'grid.dart';
+import 'line.dart';
+
 class Day5Part1 extends Solver {
   Day5Part1() : super(5);
 
   @override
   String solve(List<String> input) {
-    return "1";
+    var largestX = 0, largestY = 0;
+    final axialLines = input
+        .map((s) => AxialLine.fromString(s))
+        .where((line) => line.isHorizontal || line.isVertical);
+    axialLines.forEach((l) {
+      if (l.start.x > largestX) largestX = l.start.x;
+      if (l.end.x > largestX) largestX = l.end.x;
+      if (l.start.y > largestY) largestY = l.start.y;
+      if (l.start.y > largestY) largestY = l.start.y;
+    });
+    final grid = Grid.fromDimension(largestX + 1, largestY + 1);
+    axialLines.forEach((line) => grid.markAxialLine(line));
+
+    return grid.countOverlaps().toString();
   }
 }
