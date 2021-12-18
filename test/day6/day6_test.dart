5155db65cf7e354ae06e223cd067c2830f62fd75
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-18 12:28:30 +0100

    test p1 solution d6

---
+++ test/day6/day6_test.dart
@@ -0,0 +1,24 @@
+import 'package:advent_of_code_21/utils.dart';
+import 'package:advent_of_code_21/solver.dart';
+import 'package:test/test.dart';
+
+import '../../bin/day6/p1.dart';
+import '../../bin/day6/p2.dart';
+
+void main() {
+  late Solver solver;
+  late List<String> sampleInput;
+  setUp(() {
+    sampleInput = readSampleInput(6);
+  });
+  test("test d6p1's solve method", () {
+    solver = Day6Part1();
+    final res = solver.solve(sampleInput);
+    expect(res, equals("5934"));
+  });
+  // test("test d6p2's solve method", () {
+  //   solver = Day6Part2();
+  //   final res = solver.solve(sampleInput);
+  //   expect(res, equals("12"));
+  // });
+}
