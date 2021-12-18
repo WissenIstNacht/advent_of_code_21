3372c295961f2b6eab3a6513455a5ccb345ac2e5
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-18 12:25:45 +0100

    implement p1 solution d6

--- bin/day6/p1.dart
+++ bin/day6/p1.dart
@@ -1,10 +1,19 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/school.dart';
+
 class Day6Part1 extends Solver {
   Day6Part1() : super(6);
 
   @override
   String solve(List<String> input) {
-    return "6-1";
+    final parsedInput = input[0].split(",").map((s) => int.parse(s)).toList();
+    final school = School.fromInts(parsedInput);
+
+    while (school.age < 80) {
+      school.evolve();
+    }
+
+    return school.countPopulation().toString();
   }
 }
