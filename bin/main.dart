c86caf6d9ecc9426dba93f7d15ed262bfd35d188
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-19 22:40:57 +0100

    d7: solve p1

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,8 @@
 import 'package:advent_of_code_21/executor.dart';
 
-import 'day6/p2.dart';
+import 'day7/p1.dart';
 
 void main(List<String> arguments) {
-  var solver = Day6Part2();
+  var solver = Day7Part1();
   Executor.execute(solver);
 }
