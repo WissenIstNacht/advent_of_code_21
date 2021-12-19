b2d68191acd8d6296d9eb29ccd664558ea8804ca
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-19 23:05:00 +0100

    d7: implement solution p2

--- bin/day7/p2.dart
+++ bin/day7/p2.dart
@@ -5,6 +5,25 @@ class Day7Part2 extends Solver {
 
   @override
   String solve(List<String> input) {
-    return "2";
+    final positions = input[0].split(",").map((s) => int.parse(s));
+    final total = positions.reduce((a, b) => a + b);
+    print(total);
+    final avg = (total / positions.length).round();
+
+    var fuel = 0;
+    for (var pos in positions) {
+      fuel += computeFuel(pos, avg);
+    }
+    print(fuel);
+    return "5";
+  }
+
+  int computeFuel(from, to) {
+    final d = (from - to).abs();
+    var fuel = 0;
+    for (var i = 1; i <= d; i++) {
+      fuel += i;
+    }
+    return fuel;
   }
 }
