2a523816b7d8b5982f524313ebf02556d77fbace
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-19 21:37:10 +0100

    d7: prepare tests

---
+++ test/day7/day7_test.dart
@@ -0,0 +1,24 @@
+import 'package:advent_of_code_21/utils.dart';
+import 'package:advent_of_code_21/solver.dart';
+import 'package:test/test.dart';
+
+import '../../bin/day7/p1.dart';
+import '../../bin/day7/p2.dart';
+
+void main() {
+  late Solver solver;
+  late List<String> sampleInput;
+  setUp(() {
+    sampleInput = readSampleInput(7);
+  });
+  test("test d7p1's solve method", () {
+    solver = Day7Part1();
+    final res = solver.solve(sampleInput);
+    expect(res, equals("1"));
+  });
+  // test("test d7p2's solve method", () {
+  //   solver = Day7Part2();
+  //   final res = solver.solve(sampleInput);
+  //   expect(res, equals("2"));
+  // });
+}
