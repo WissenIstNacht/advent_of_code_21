5f389867e856880cd829209e06cff7db6b9e3b22
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-19 22:35:54 +0100

    d7: implement solution p1

--- bin/day7/p1.dart
+++ bin/day7/p1.dart
@@ -1,10 +1,19 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/converger.dart';
+
 class Day7Part1 extends Solver {
   Day7Part1() : super(7);
 
   @override
   String solve(List<String> input) {
-    return "1";
+    final positions = input[0].split(",").map((s) => int.parse(s)).toList();
+    final converger = Converger.fromPositions(positions);
+
+    while (!converger.hasConverged()) {
+      converger.converge();
+    }
+
+    return converger.fuel.toString();
   }
 }
