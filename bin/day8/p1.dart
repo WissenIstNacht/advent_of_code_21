e71664cc255a8ec269648a35eba7277b7b5dd5ba
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-21 10:21:29 +0100

    d8: implement solution p1

--- bin/day8/p1.dart
+++ bin/day8/p1.dart
@@ -5,6 +5,20 @@ class Day8Part1 extends Solver {
 
   @override
   String solve(List<String> input) {
-    return "1";
+    final outputs =
+        input.map((s) => s.split("|")).map((pair) => pair.last.trim());
+
+    var digitsCount = 0;
+    for (var output in outputs) {
+      final relevantDigits = output.split(" ").where((digitSignal) {
+        return digitSignal.length == 2 || // uniquely identifies digit 1
+            digitSignal.length == 3 || // uniquely identifies digit 7
+            digitSignal.length == 4 || // uniquely identifies digit 4
+            digitSignal.length == 7; // uniquely identifies digit 8
+      });
+      digitsCount += relevantDigits.length;
+    }
+
+    return digitsCount.toString();
   }
 }
