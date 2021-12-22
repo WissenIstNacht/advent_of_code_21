70e76320e6fe09f9d92d0d618e300f14c1b7830f
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-22 23:07:58 +0100

    d8: implement solution p2

--- bin/day8/p2.dart
+++ bin/day8/p2.dart
@@ -1,10 +1,18 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/display.dart';
+
 class Day8Part2 extends Solver {
   Day8Part2() : super(8);
 
   @override
   String solve(List<String> input) {
-    return "2";
+    final displays = input.map((l) => Display.parse(l));
+    var counter = 0;
+    for (var display in displays) {
+      display.decodeInputs();
+      counter += display.decodeOutputs();
+    }
+    return counter.toString();
   }
 }
