6366501a7dcd2eda8b829dfc1e05b0f76289264a
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-25 12:55:46 +0100

    d9: implement solution p1

--- bin/day9/p1.dart
+++ bin/day9/p1.dart
@@ -1,10 +1,16 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/heightMap.dart';
+
 class Day9Part1 extends Solver {
   Day9Part1() : super(9);
 
   @override
   String solve(List<String> input) {
-    return "1";
+    final heightMap = HeightMap.parse(input);
+    final points = heightMap.findLowPoints();
+    final res = points.map((p) => p += 1).reduce((a, b) => a + b);
+
+    return res.toString();
   }
 }
