7c7c86b7dde71b28b84bdb39fc73b57a3cc5c1dc
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-25 20:45:49 +0100

    d9: implement p2

--- bin/day9/p2.dart
+++ bin/day9/p2.dart
@@ -1,10 +1,22 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/heightMap.dart';
+
 class Day9Part2 extends Solver {
   Day9Part2() : super(9);
 
   @override
   String solve(List<String> input) {
-    return "2";
+    final heightMap = HeightMap.parse(input);
+    final points = heightMap.findLowPoints();
+    final basinLengths =
+        points.map((p) => heightMap.findBasin(p)).map((b) => b.length).toList();
+    basinLengths.sort();
+    final largestBasins =
+        basinLengths.sublist(basinLengths.length - 3, basinLengths.length);
+    assert(largestBasins.length == 3);
+    final res = largestBasins.reduce((a, b) => a * b);
+
+    return res.toString();
   }
 }
