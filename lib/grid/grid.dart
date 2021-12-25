dbbb8ce95845083f6b771d1b3674123ad7516f38
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-25 23:27:30 +0100

    add grid class

---
+++ lib/grid/grid.dart
@@ -0,0 +1,36 @@
+class Grid<T> {
+  late int width, height;
+  late List<List<T>> cells;
+
+  /* CONSTRUCTOR ============================================================ */
+
+  Grid();
+
+  Grid.empty(int width, int height) {
+    this.width = width;
+    this.height = height;
+    List.filled(height, List.filled(width, null));
+  }
+
+  Grid.filled(int width, int height, T val) {
+    this.width = width;
+    this.height = height;
+    List.filled(height, List.filled(width, val));
+  }
+
+  /* QUERY ================================================================== */
+
+  T getCell(int x, int y) => cells[y][x];
+
+  bool outOfGrid(int x, int y) => x < 0 || x >= width || y < 0 || y >= height;
+
+  /* UTILS ================================================================== */
+
+  List<List<S>> map<S>(S Function(T) f) {
+    return cells.map((List<T> r) => r.map(f).toList()).toList();
+  }
+
+  void forEach(Function(T) f) {
+    return cells.forEach((List<T> r) => r.forEach(f));
+  }
+}
