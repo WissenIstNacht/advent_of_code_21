e53477d0a06b38f3756cd811d3275f73166f34fe
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-27 19:48:06 +0100

    d10: prepare

---
+++ bin/day10/p1.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day10Part1 extends Solver {
+  Day10Part1() : super(10);
+
+  @override
+  String solve(List<String> input) {
+    return "1";
+  }
+}
---
+++ bin/day10/p2.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day10Part2 extends Solver {
+  Day10Part2() : super(10);
+
+  @override
+  String solve(List<String> input) {
+    return "2";
+  }
+}
