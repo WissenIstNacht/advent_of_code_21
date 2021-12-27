0d38011fb2738e61e8bf1e0eb849568229b3cb9d
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-27 20:39:48 +0100

    d10: prepare tests

---
+++ test/day10/day9_test.dart
@@ -0,0 +1,24 @@
+import 'package:advent_of_code_21/utils.dart';
+import 'package:advent_of_code_21/solver.dart';
+import 'package:test/test.dart';
+
+import '../../bin/day10/p1.dart';
+import '../../bin/day10/p2.dart';
+
+void main() {
+  late Solver solver;
+  late List<String> sampleInput;
+  setUp(() {
+    sampleInput = readSampleInput(10);
+  });
+  test("test d10p1's solve method", () {
+    solver = Day10Part1();
+    final res = solver.solve(sampleInput);
+    expect(res, equals("15"));
+  });
+  test("test d10p2's solve method", () {
+    solver = Day10Part2();
+    final res = solver.solve(sampleInput);
+    expect(res, equals("1134"));
+  });
+}
