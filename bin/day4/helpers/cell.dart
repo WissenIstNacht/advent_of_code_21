9597884d82384a236858d0d47a17de7424598eb7
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2021-12-28 22:03:15 +0100

    move cell mixins

--- bin/day4/helpers/cell.dart
+++ bin/day4/helpers/cell.dart
@@ -1,6 +1,5 @@
 import 'package:advent_of_code_21/grid/cell.dart';
-
-import '../../day9/helpers/markable.dart';
+import 'package:advent_of_code_21/grid/markable.dart';
 
 class Number extends Cell<int> with Markable {
   Number.parse(String s) : super(int.parse(s)) {
--- bin/day9/helpers/cell.dart
+++ bin/day9/helpers/cell.dart
@@ -1,6 +1,6 @@
 import 'package:advent_of_code_21/grid/cell.dart';
-import 'markable.dart';
-import 'positionable.dart';
+import 'package:advent_of_code_21/grid/markable.dart';
+import 'package:advent_of_code_21/grid/positionable.dart';
 
 class Height extends Cell<int> with Markable, Positionable {
   Height.parseWithPosition(String s, int x, int y) : super(int.parse(s)) {
