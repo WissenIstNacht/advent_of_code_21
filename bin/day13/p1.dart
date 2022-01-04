fda1bb885fd11927c09057e6811ae4b836ebb6a2
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-01-04 22:10:26 +0100

    d13: prepare

---
+++ bin/day13/p1.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day13Part1 extends Solver {
+  Day13Part1() : super(13);
+
+  @override
+  String solve(List<String> input) {
+    return "2";
+  }
+}
---
+++ bin/day13/p2.dart
@@ -0,0 +1,10 @@
+import 'package:advent_of_code_21/solver.dart';
+
+class Day13Part2 extends Solver {
+  Day13Part2() : super(13);
+
+  @override
+  String solve(List<String> input) {
+    return "2";
+  }
+}
