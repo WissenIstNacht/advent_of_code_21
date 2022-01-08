e430cb43b0eae9c71abe39b7e19f1ada33ed67ce
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2022-01-08 21:01:29 +0100

    d13: implement solution p1

--- bin/day13/p1.dart
+++ bin/day13/p1.dart
@@ -1,10 +1,34 @@
+import 'package:advent_of_code_21/grid/markable.dart';
 import 'package:advent_of_code_21/solver.dart';
+
+import 'helpers/instruction.dart';
+import 'helpers/paper.dart';
 
 class Day13Part1 extends Solver {
   Day13Part1() : super(13);
 
   @override
   String solve(List<String> input) {
-    return "2";
+    final coordinates = <String>[];
+    final instructions = <FoldInstruction>[];
+
+    for (var i in input) {
+      if (i.isEmpty) {
+        // delimiter between coordinates and instructions
+        continue;
+      } else if (i.startsWith("f")) {
+        instructions.add(FoldInstruction.parse(i));
+      } else {
+        coordinates.add(i);
+      }
+    }
+
+    final paper = Paper.fromCoordinates(coordinates);
+    final paperPrint = paper.map((c) => (c as Markable).marked ? "#" : ".");
+    paperPrint.forEach(print);
+
+    paper.fold(instructions.first);
+
+    return paper.countMarked().toString();
   }
 }
