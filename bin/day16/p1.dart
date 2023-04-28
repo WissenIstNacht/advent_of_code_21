ddcfcea6bf4cae811f6014d19e0d1db3e28fece9
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-04-28 15:24:34 +0200

    d16: implement & test solution p1

--- bin/day16/p1.dart
+++ bin/day16/p1.dart
@@ -1,10 +1,21 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/signal.dart';
+
 class Day16Part1 extends Solver {
   Day16Part1() : super(16);
 
   @override
   String solve(List<String> input) {
-    return "42";
+    final signal = Signal(input.first);
+    final apt = signal.consumeSignal();
+
+    return apt.versionNumberSum().toString();
   }
+}
+
+List<String> takeAndRemove(List<String> list, int count) {
+  final sublist = list.take(count).toList();
+  list.removeRange(0, count);
+  return sublist;
 }
--- test/day16/day16_test.dart
+++ test/day16/day16_test.dart
@@ -1,18 +1,27 @@
-import 'package:advent_of_code_21/solver.dart';
 import 'package:advent_of_code_21/utils.dart';
 import 'package:test/test.dart';
 
 import '../../bin/day16/p1.dart';
 
 void main() {
-  late Solver solver;
-  late List<String> sampleInput;
-  setUp(() {
-    sampleInput = readSampleInput(15);
+  test("d16 p1 solve method 1", () {
+    final solver = Day16Part1();
+    final res = solver.solve(["8A004A801A8002F478"]);
+    expect(res, equals("16"));
   });
-  test("test d16p1's solve method", () {
-    solver = Day16Part1();
-    final res = solver.solve(sampleInput);
-    expect(res, equals("42"));
+  test("d16 p1 solve method 2", () {
+    final solver = Day16Part1();
+    final res = solver.solve(["620080001611562C8802118E34"]);
+    expect(res, equals("12"));
+  });
+  test("d16 p1 solve method 3", () {
+    final solver = Day16Part1();
+    final res = solver.solve(["C0015000016115A2E0802F182340"]);
+    expect(res, equals("23"));
+  });
+  test("d16 p1 solve method 4", () {
+    final solver = Day16Part1();
+    final res = solver.solve(["A0016C880162017C3686B18A3D4780"]);
+    expect(res, equals("31"));
   });
 }
