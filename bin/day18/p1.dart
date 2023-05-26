1b75e4c617bdb87db80bdce6067fb318f137de4f
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-05-26 15:39:40 +0200

    d18: implement summation function in p1

--- bin/day18/p1.dart
+++ bin/day18/p1.dart
@@ -1,10 +1,24 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/parser.dart';
+import 'helpers/sfn.dart';
+
 class Day18Part1 extends Solver {
   Day18Part1() : super(18);
 
   @override
   String solve(List<String> input) {
-    return "42";
+    var sfns = input.map(parse).toList();
+    var res = sum(sfns);
+    return sfns.length.toString();
+  }
+
+  SFN sum(List<SFN> sfns) {
+    // assuming there is at least 3 numbers.
+    var res = sfns.first;
+    for (var i = 1; i < sfns.length; i++) {
+      res = res.add(sfns[i]);
+    }
+    return res;
   }
 }
--- test/day18/day18_test.dart
+++ test/day18/day18_test.dart
@@ -1,18 +1,20 @@
-import 'package:advent_of_code_21/solver.dart';
 import 'package:advent_of_code_21/utils.dart';
 import 'package:test/test.dart';
 
+import '../../bin/day18/helpers/parser.dart';
 import '../../bin/day18/p1.dart';
 
 void main() {
-  late Solver solver;
   late List<String> sampleInput;
   setUp(() {
     sampleInput = readSampleInput(18);
   });
-  test("test d17p1's solve method", () {
-    solver = Day18Part1();
-    final res = solver.solve(sampleInput);
-    expect(res, equals("42"));
+  test("test d18p1's solve method", () {
+    final solver = Day18Part1();
+    var sfns = sampleInput.map(parse).toList();
+
+    final res = solver.sum(sfns);
+    expect(res.toString(),
+        equals("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"));
   });
 }
