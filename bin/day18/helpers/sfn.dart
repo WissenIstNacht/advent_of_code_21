99be25a4d2d4ae2c86670ae3aa172e8b32d6a2fb
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-05-26 08:53:26 +0200

    d18: implement SFN addition

--- bin/day18/helpers/sfn.dart
+++ bin/day18/helpers/sfn.dart
@@ -8,6 +8,153 @@ class SFN {
 
   SFN(this.l, this.r);
 
+  SFN add(SFN other) {
+    var newSfn = SFN(Right<int, SFN>(this), Right<int, SFN>(other));
+    newSfn.l.right.parent = newSfn;
+    newSfn.r.right.parent = newSfn;
+    newSfn.reduce();
+    return newSfn;
+  }
+
+  SFN? shouldExplode([int depth = 0]) {
+    if (l.isRight) {
+      var toExplode = l.right.shouldExplode(depth + 1);
+      if (toExplode != null) return toExplode;
+    }
+
+    if (r.isRight) {
+      var toExplode = r.right.shouldExplode(depth + 1);
+      if (toExplode != null) return toExplode;
+    }
+
+    if (depth >= 4) {
+      return this;
+    } else {
+      return null; // reached a leaf but it's not nested deelpy enough
+    }
+  }
+
+  void explode() {
+    // find first ancestor whose left child is a regular number
+    SFN? curr = this;
+    while (curr != null) {
+      if (curr.isRightChild()) {
+        var p = curr.parent!;
+        if (p.l.isLeft) {
+          p.l = Left(p.l.left + l.left);
+          break;
+        }
+        var node = p.l.right.findRightestLeaf();
+        node.r = Left(node.r.left + l.left);
+        break;
+      }
+      curr = curr.parent;
+    }
+    // find first ancestor whose right child is a regular number
+    curr = this;
+    while (curr != null) {
+      if (curr.isLeftChild()) {
+        var p = curr.parent!;
+        if (p.r.isLeft) {
+          p.r = Left(p.r.left + r.left);
+          break;
+        }
+        var node = p.r.right.findLeftestLeaf();
+        node.l = Left(node.l.left + r.left);
+        break;
+      }
+      curr = curr.parent;
+    }
+
+    if (isLeftChild()) {
+      parent!.l = Left<int, SFN>(0);
+    }
+    if (isRightChild()) {
+      parent!.r = Left<int, SFN>(0);
+    }
+  }
+
+  SFN findLeftestLeaf() {
+    if (l.isLeft) {
+      return this;
+    } else {
+      return l.right.findLeftestLeaf();
+    }
+  }
+
+  SFN findRightestLeaf() {
+    if (r.isLeft) {
+      return this;
+    } else {
+      return r.right.findLeftestLeaf();
+    }
+  }
+
+  SFN? shouldSplit() {
+    if (l.isLeft && l.left >= 10) {
+      return this;
+    }
+    if (r.isLeft && r.left >= 10) {
+      return this;
+    }
+
+    if (l.isRight) {
+      var toSplit = l.right.shouldSplit();
+      if (toSplit != null) return toSplit;
+    }
+
+    if (r.isRight) {
+      var toSplit = r.right.shouldSplit();
+      if (toSplit != null) return toSplit;
+    }
+  }
+
+  void split() {
+    if (l.isLeft && l.left >= 10) {
+      var newL = l.left ~/ 2;
+      var newR = l.left - newL;
+      var newNode = SFN(Left(newL), Left(newR));
+      l = Right(newNode);
+      newNode.parent = this;
+    }
+    if (r.isLeft && r.left >= 10) {
+      var newL = r.left ~/ 2;
+      var newR = r.left - newL;
+      var newNode = SFN(Left(newL), Left(newR));
+      r = Right(newNode);
+      newNode.parent = this;
+    }
+  }
+
+  void reduce() {
+    while (true) {
+      var toExplode = shouldExplode();
+      if (toExplode != null) {
+        toExplode.explode();
+        continue;
+      }
+
+      var toSplit = shouldSplit();
+      if (toSplit != null) {
+        toSplit.split();
+        continue;
+      }
+      return;
+    }
+  }
+
+  bool isLeftChild() {
+    if (parent == null) return false;
+    if (parent!.l.isRight && parent!.l.right == this) return true;
+    return false;
+  }
+
+  bool isRightChild() {
+    if (parent == null) return false;
+    if (parent!.r.isRight && parent!.r.right == this) return true;
+    return false;
+  }
+
   @override
   String toString() {
     var leftString = "";
--- test/day18/sfn_test.dart
+++ test/day18/sfn_test.dart
@@ -28,4 +28,128 @@ void main() {
     final res = sfn.toString();
     expect(res, equals("[[[[[1,2],3],4],[5,[[6,7],8]]],[9,0]]"));
   });
+  test("test shouldExplode() on '[[[[[1,2],3],4],[5,[[6,7],8]]],[9,0]]'", () {
+    const input = "[[[[[1,2],3],4],[5,[[6,7],8]]],[9,0]]";
+    final sfn = parse(input);
+    final res = sfn.shouldExplode();
+    expect(res.toString(), equals("[1,2]"));
+  });
+  test("test explode() on '[[[[[1,2],3],4],[5,[[6,7],8]]],[9,0]]'", () {
+    const input = "[[[[[1,2],3],4],[5,[[6,7],8]]],[9,0]]";
+    final sfn = parse(input);
+    final toExplode = sfn.shouldExplode();
+    toExplode!.explode();
+    expect(sfn.toString(), equals("[[[[0,5],4],[5,[[6,7],8]]],[9,0]]"));
+  });
+  test("test shouldExplode() on '[[[[0,5],4],[5,[[6,7],8]]],[9,0]]'", () {
+    const input = "[[[[0,5],4],[5,[[6,7],8]]],[9,0]]";
+    final sfn = parse(input);
+    final res = sfn.shouldExplode();
+    expect(res.toString(), equals("[6,7]"));
+  });
+  test("test explode() on '[[[[0,5],4],[5,[[6,7],8]]],[9,0]]'", () {
+    const input = "[[[[0,5],4],[5,[[6,7],8]]],[9,0]]";
+    final sfn = parse(input);
+    final toExplode = sfn.shouldExplode();
+    toExplode!.explode();
+    expect(sfn.toString(), equals("[[[[0,5],4],[11,[0,15]]],[9,0]]"));
+  });
+  test("test explode() on '[[[[[9,8],1],2],3],4]'", () {
+    const input = "[[[[[9,8],1],2],3],4]";
+    final sfn = parse(input);
+    final toExplode = sfn.shouldExplode();
+    toExplode!.explode();
+    expect(sfn.toString(), equals("[[[[0,9],2],3],4]"));
+  });
+  test("test explode() on '[7,[6,[5,[4,[3,2]]]]]'", () {
+    const input = "[7,[6,[5,[4,[3,2]]]]]";
+    final sfn = parse(input);
+    final toExplode = sfn.shouldExplode();
+    toExplode!.explode();
+    expect(sfn.toString(), equals("[7,[6,[5,[7,0]]]]"));
+  });
+  test("test explode() on '[[6,[5,[4,[3,2]]]],1]'", () {
+    const input = "[[6,[5,[4,[3,2]]]],1]";
+    final sfn = parse(input);
+    final toExplode = sfn.shouldExplode();
+    toExplode!.explode();
+    expect(sfn.toString(), equals("[[6,[5,[7,0]]],3]"));
+  });
+  test("test shouldSplit() on '[17,1]'", () {
+    const input = "[7,1]";
+    final sfn = parse(input);
+    sfn.l = Left(17);
+    final res = sfn.shouldSplit();
+    expect(res.toString(), equals("[17,1]"));
+  });
+  test("test shouldSplit() on '[1,[17,1]]'", () {
+    const input = "[1,[7,1]]";
+    final sfn = parse(input);
+    sfn.r.right.l = Left(17);
+    final res = sfn.shouldSplit();
+    expect(res.toString(), equals("[17,1]"));
+  });
+  test("test shouldSplit() on '[[17,1],1]'", () {
+    const input = "[[7,1],1]";
+    final sfn = parse(input);
+    sfn.l.right.l = Left(17);
+    final res = sfn.shouldSplit();
+    expect(res.toString(), equals("[17,1]"));
+  });
+  test("test split() on '[17,1]'", () {
+    const input = "[7,1]";
+    final sfn = parse(input);
+    sfn.l = Left(17);
+    final toSplit = sfn.shouldSplit();
+    toSplit!.split();
+    expect(sfn.toString(), equals("[[8,9],1]"));
+  });
+  test("test shouldSplit() on '[1,[17,1]]'", () {
+    const input = "[1,[7,1]]";
+    final sfn = parse(input);
+    sfn.r.right.l = Left(17);
+    final toSplit = sfn.shouldSplit();
+    toSplit!.split();
+    expect(sfn.toString(), equals("[1,[[8,9],1]]"));
+  });
+  test(
+      "test explode() followed by split() on '[[[[0,5],4],[5,[[6,7],8]]],[9,0]]'",
+      () {
+    const input = "[[[[0,5],4],[5,[[6,7],8]]],[9,0]]";
+    final sfn = parse(input);
+    final toExplode = sfn.shouldExplode();
+    toExplode!.explode();
+    final toSplit = sfn.shouldSplit();
+    toSplit!.split();
+    expect(sfn.toString(), equals("[[[[0,5],4],[[5,6],[0,15]]],[9,0]]"));
+  });
+  test("test reduce() on '[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'", () {
+    const input = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]";
+    final sfn = parse(input);
+    sfn.reduce();
+    expect(sfn.toString(), equals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"));
+  });
+  test("test '[[[[[4,3],4],4],[7,[[8,4],9]]]' + '[1,1]'", () {
+    const input1 = "[[[[4,3],4],4],[7,[[8,4],9]]]";
+    const input2 = "[1,1]";
+
+    final sfn1 = parse(input1);
+    final sfn2 = parse(input2);
+
+    final res = sfn1.add(sfn2);
+    expect(res.toString(), equals("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"));
+  });
+  test(
+      "test [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]] + [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]",
+      () {
+    const input1 = "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]";
+    const input2 = "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]";
+
+    final sfn1 = parse(input1);
+    final sfn2 = parse(input2);
+
+    final res = sfn1.add(sfn2);
+    expect(res.toString(),
+        equals("[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"));
+  });
 }
