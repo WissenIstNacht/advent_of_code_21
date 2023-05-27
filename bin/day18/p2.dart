de7e1aa999446c89ef3418d4606c941550210962
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-05-27 13:15:01 +0200

    d18: solve p2

--- bin/day18/p2.dart
+++ bin/day18/p2.dart
@@ -1,10 +1,27 @@
 import 'package:advent_of_code_21/solver.dart';
 
+import 'helpers/parser.dart';
+
 class Day18Part2 extends Solver {
   Day18Part2() : super(18);
 
   @override
   String solve(List<String> input) {
-    return "42";
+    var maxMagnitude = 0;
+    for (var i = 0; i < input.length; i++) {
+      for (var j = 0; j < input.length; j++) {
+        if (i == j) {
+          continue;
+        }
+        final sfni = parse(input[i]);
+        final sfnj = parse(input[j]);
+
+        final currMagnitude = sfni.add(sfnj).magnitude();
+        if (currMagnitude > maxMagnitude) {
+          maxMagnitude = currMagnitude;
+        }
+      }
+    }
+    return maxMagnitude.toString();
   }
 }
--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,9 @@
 import 'package:advent_of_code_21/executor.dart';
 
-import 'day18/p1.dart';
+import 'day18/p2.dart';
 
 void main(List<String> arguments) {
-  var solver = Day18Part1();
+  var solver = Day18Part2();
 
   Executor.execute(solver);
 }
